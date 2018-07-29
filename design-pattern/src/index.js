e50c82b4e844e2557d113f18fc4bf2bfd5a5a640
湳有乔沐 <cwzp990@163.com>
2018-07-29 17:22:14 +0800

    uml

--- design-pattern/src/index.js
+++ design-pattern/src/index.js
@@ -1,12 +1,52 @@
-class Person {
-  constructor(name) {
-    this.name = name
+// class Person {
+//   constructor(name, age) {
+//     this.name = name
+//     this.age = age
+//   }
+//   eat() {
+//     alert(`${this.name} is eat somethings`)
+//   }
+// }
+
+// class Student extends Person {
+//   constructor(name, age, number) {
+//     super(name, age)
+//     this.number = number
+//   }
+//   study() {
+//     alert(`${this.name} is study!`)
+//   }
+// }
+
+// let xiaoming = new Student('xiaoming', 20, 'a1')
+
+// let xiaohong = new Student('xiaohong', 21, 'a2')
+
+// xiaoming.study()
+
+class jQuery {
+  constructor(selector) {
+    let slice = Array.prototype.slice
+    let dom = slice.call(document.querySelectorAll(selector))
+    let len = dom ? dom.length : 0
+    for (var i = 0; i < len; i++) {
+      this[i] = dom[i]
+    }
+    this.length = len
+    this.selector = selector || ''
   }
-  getName() {
-    return this.name
+  append(mode) {
+    // ......
+  }
+  html(data) {
+    // ......
   }
 }
 
-let p = new Person('tom')
+window.$ = function(selector) {
+  return new jQuery(selector)
+}
 
-alert(p.getName())
+var $p = $('p')
+console.log($p)
+console.log($p.html)
