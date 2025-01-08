aae6728a2e2af22ac263e6ccdb876db7b01e82e8
cwzp990 <cwzp990@163.com>
2025-01-08 18:35:45 +0800

    fix: translate notes

--- interview/webgl/translate.js
+++ interview/webgl/translate.js
@@ -1,33 +1,51 @@
 const canvas = document.querySelector("canvas");
-canvas.width = canvas.height = 300;
+canvas.width = canvas.height = 800;
 document.body.appendChild(canvas);
 const ctx = canvas.getContext("2d");
 
 const transform = () => {}; // 后面调用的转换函数
 
-const shape = [
+const rect = [
   [0, 0],
   [0, 100],
   [100, 100],
   [100, 0],
 ];
 ctx.beginPath();
-shape.forEach((p) => ctx.lineTo(...transform(p)));
+rect.forEach((p) => ctx.lineTo(...transform(p)));
 ctx.closePath;
 ctx.fillStyle = "rgba(0,255,255,1)";
 ctx.fill();
 
-// 平移
+/**
+ * 平移
+ * @param x 横坐标
+ * @param y 纵坐标
+ * @param dx x轴偏移量
+ * @param dy y轴偏移量
+ */
 function translate([x, y], dx = 0, dy = 0) {
   return [x + dx, y + dy];
 }
 
-// 缩放
+/**
+ * 缩放
+ * @param x 横坐标
+ * @param y 纵坐标
+ * @param xs x轴缩放系数
+ * @param ys y轴缩放系数，默认等于xs，即等比例缩放
+ */
 function scale([x, y], xs = 1, ys = xs) {
   return [x * xs, y * ys];
 }
 
-// 错切
+/**
+ * 错切
+ * @param x 横坐标
+ * @param y 纵坐标
+ * @param sx x轴偏移角度，需转换成弧度
+ * @param ys y轴偏移角度，需转换成弧度
+ */
 function skew([x, y], sx = 0, sy = 0) {
   const rad = (r) => (r * Math.PI) / 180; // 角度转弧度
   return [x + Math.tan(rad(sx)) * y, y + Math.tan(rad(sy)) * y]; // tan 对边/临边
